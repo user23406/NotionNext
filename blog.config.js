2cc0ad29d46a9b202a10c40642b7a339ddf1160f
user23406 <116787914+user23406@users.noreply.github.com>
2023-11-30 19:20:26 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,8 +17,8 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '3S Group', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || 'Sok-paper Systematic-learning Sharing group', // 作者简介
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Aria', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || 'Focus on Blockchain,Web3,English Learning', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion-next-three-neon.vercel.app/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
